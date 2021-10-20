c42058cb949eb4f6d6d6c93645c05744b58460db
Nia K <nsjkappen@gmail.com>
2021-10-20 09:12:56 -0700

    mainpage

--- src/components/Ideasdisplay.js
+++ src/components/Ideasdisplay.js
@@ -2,17 +2,14 @@
 import React, {useState} from 'react'
 
 function Ideasdisplay() {
-    const [ideas, setIdeas] = useState("");
-
-    const getIdeas = () => {
+    
 
        
 
     return (
         <div>
 
-        Hello ideas <button onClick={getIdeas}>Now </button>
-        {ideas}
+       
         </div>
     )
 }
