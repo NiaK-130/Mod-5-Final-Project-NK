54da26f606a22b3d4796a115a612b182ed357795
Nia K <nsjkappen@gmail.com>
2021-10-27 11:01:53 -0700

    hidden yelp api key

--- src/hooks/yelp-api/api.js
+++ src/hooks/yelp-api/api.js
@@ -1,11 +1,11 @@
-import { API_BASE_URL, BEARER_TOKEN } from "./config";
+import { API_BASE_URL, BASE_KEY_API } from "./config";
 import queryString from 'query-string'
 
 export function get(path, queryParams){
     const query = queryString.stringify(queryParams);
     return fetch(`${API_BASE_URL}${path}?${query}`, {
         headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
+            Authorization: `Bearer ${BASE_KEY_API}`,
             Origin: 'localhost',
             withCredentials: true,
         }
--- src/hooks/yelp-api/config.js
+++ src/hooks/yelp-api/config.js
@@ -1,5 +1,3 @@
 export const API_BASE_URL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/`;
 
-export const BEARER_TOKEN = 'SveB1y595ibHXmbzk8RwS4YVpBX_wMurAVYGxtmkjN5hsLlE0XU9OqlR9S3n8hQ2JEHxjQGRMD5ueAaJAvSw-Y1HuzK5iuGajr5_jt3dWfmzyv4oUIF-SORUVMVwYXYx';
-
-
+export const BASE_KEY_API = process.env.REACT_APP_YELP_ACCESSKEY 
